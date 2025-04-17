5978d176fe956e395d04490d022b123148c0eef1
Farzana Akter <farzana.nilu99@gmail.com>
2025-04-17 12:25:40 +0600

    last update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,13 @@
-import { Component, NgModule } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./navbar/navbar.component";
-import { DoctorsComponent } from "./doctors/doctors.component";
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FooterComponent } from "./footer/footer.component";
 
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, FooterComponent],
+  imports: [RouterOutlet, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
